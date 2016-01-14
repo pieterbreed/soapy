94d894743cf581afeb2c6f63a41920b8392d8e6e
Pieter Breed <github.com@pb.co.za>
2016-01-14 12:19:24 +0200

    added a check for nginx serving a marker file from the static source correctly

--- src/bash/check_nginx_is_running.sh
+++ src/bash/check_nginx_is_running.sh
@@ -2,4 +2,15 @@
 set -x
 set -e
 
-curl "http://$WEBSERVER_PUBLIC_DNSNAME:$WEBSERVER_PUBLIC_PORT" > /dev/null 2>&1
+WHEREAMI="`dirname \"$BASH_SOURCE\"`"
+WHEREAMI="`(cd \"$WHEREAMI\" && pwd)`"
+
+BASE_URL="$WEBSERVER_PUBLIC_SCHEME://$WEBSERVER_PUBLIC_DNSNAME:$WEBSERVER_PUBLIC_PORT"
+
+# is nginx up and responding on the port at all?
+curl "$BASE_URL" > /dev/null 2>&1
+
+# is the nginx serving static content from hugo at all?
+test "`cat \"$WHEREAMI/../hugo/static/soapy.marker\"`" = "`curl \"$BASE_URL/soapy.marker\" 2> /dev/null`"
+
+
