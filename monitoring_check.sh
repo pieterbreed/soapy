1b83e49f4004b31821d2cb8ba2dc78b7f7620517
Pieter Breed <github.com@pb.co.za>
2016-01-14 11:32:42 +0200

    SUCCESS message in the monitoring script

--- monitoring_check.sh
+++ monitoring_check.sh
@@ -3,7 +3,11 @@
 set -e
 set -x
 
-test ".$SOAPY_CONFIG_HAS_BEEN_LOADED" = ".1" # environment loaded at all?
+ENV_LOADED_ERROR="Did you load the environment?"
+
+test "$ENV_LOADED_ERROR.$SOAPY_CONFIG_HAS_BEEN_LOADED" = "$ENV_LOADED_ERROR.1" # environment loaded at all?
 
 # now just run the checking scripts one by one
 src/bash/check_nginx_is_running.sh
+
+echo -e "########################################\n# IF YOU SEE THIS, EVERYTHING IS COOL! #\n########################################"
