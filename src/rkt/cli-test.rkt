ea18e1b8f2f417b56f6acbdfad953cdf60692e13
Pieter Breed <github.com@pb.co.za>
2016-01-27 11:50:40 +0200

    save game

--- src/rkt/cli-test.rkt
+++ src/rkt/cli-test.rkt
@@ -6,50 +6,13 @@
          (prefix-in p: parsack)
          "cli.rkt")
 
-(define (long-string-to-list-of-strings-parser)
-  (let* ([ws " \t\n\r"]
-         [ws-p (p:many1 (p:oneOf ws))]
-         [id-p (p:parser-compose 
-                (x <- (p:many1 (p:noneOf ws)))
-                (p:return (apply string x)))])
-    (p:sepBy1 id-p ws-p)))
-
 (define cli-tests
   (test-suite
    "testing the utility functions that parses the CLI"
-   (check-equal? (p:parse-result (symbol->symbol-parser 'op)
-                                 "op")
-                 'op)
 
-   (let ([p (symbols->symbols-choice-parser 'op1 'op2 'op3)])
-     (check-equal? (p:parse-result p "op1") 'op1)
-     (check-equal? (p:parse-result p "op2") 'op2)
-     (check-equal? (p:parse-result p "op3") 'op3))
-
-   (check-equal? (p:parse-result (split-after-operator-parser)
-                                 "  op1   some other stuff ")
-                 (list 'op1 "some other stuff ")
-                 "can identify the operator and retain the params")
-   (check-equal? (p:parse-result (split-after-operator-parser)
-                                 "op2")
-                 (list 'op2 ""))
-
-   (check-equal? (p:parse-result (long-string-to-list-of-strings-parser)
-                                 "one  two three     \tfour")
-                 (list "one" "two" "three" "four")
-                 "simple parameter string")
-
-
-   (let* ([win-op (lambda x (apply list 0 x))]
-          [spec (hash 'op (vector win-op (long-string-to-list-of-strings-parser))
-                      'op2 (vector win-op (long-string-to-list-of-strings-parser))
-                      'op3 (vector win-op (long-string-to-list-of-strings-parser)))])
-     (check-equal? (run-cli spec #("op" "one" "two"))
-                   (list 0 (list "one" "two")))
-     (check-equal? (run-cli spec #("op2" "three"))
-                   (list 0 (list "three")))
-     ;; (check-equal? (run-cli spec #("op3" ""))
-     ;;               (list 0 (list "")))
-     )))
+   (let ([spec (hash
+                'op1 (p:return 1))])
+     (check-equal? (run-cli spec #("op1"))
+                   1))))
 
 (exit (run-tests cli-tests))
--- src/rkt/cli.rkt
+++ src/rkt/cli.rkt
@@ -5,42 +5,14 @@
 
 (require (prefix-in p: parsack))
 
-(define cli-one-line (string-join (vector->list (current-command-line-arguments))
-                                  " "))
-
-(define (symbol->symbol-parser s)
-  (p:>> (p:try (p:string (symbol->string s)))
-        (p:return s)))
-
-(define (symbols->symbols-choice-parser . ss)
-  (p:choice (map symbol->symbol-parser ss)))
-
-(define (split-after-operator-parser)
-  (p:parser-compose p:$spaces
-                    (op-s <- p:$identifier)
-                    p:$spaces
-                    (rest <- (p:many p:$anyChar))
-                    (p:return
-                     (list (string->symbol (apply string op-s))
-                           (apply string rest)))))
-
 (define (run-cli spec cli-params)
-  (let* ([cli-str (string-join (vector->list cli-params))]
-         [op-params (p:parse-result (split-after-operator-parser)
-                                    cli-str)]
-         [op (first op-params)]
-         [params (second op-params)]
-         [op-record (dict-ref spec op)]
-         [parser (vector-ref op-record 1)]
-         [data (p:parse-result parser params)])
-    ((vector-ref op-record 0) data)))
+  (let ([params-list (vector->list cli-params)])
+    (p:parse-result
+     (let ([parsed-op (string->symbol (first params-list))])
+       (unless (dict-has-key? spec parsed-op)
+               (error "Unkown operation!"))
+       (dict-ref spec parsed-op))
+     (rest params-list))))
 
-;; (define (find-op-parser)
-;;   (let ([ops (map symbol->symbol-parser (dict-keys options))])
-;;     (log-cli-debug "found these operators: ~s" ops)
-    
-(provide symbol->symbol-parser
-         symbols->symbols-choice-parser
-         split-after-operator-parser
-         run-cli)
+(provide run-cli)
 
