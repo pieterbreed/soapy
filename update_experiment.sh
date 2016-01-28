0b39d6cfb38ee59f970fb254a10c0f77c18740d7
Pieter Breed <github.com@pb.co.za>
2016-01-28 10:21:19 +0200

    Seeing if we can render experiment sub-domains

---
+++ update_experiment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+TOP="`dirname \"$BASH_SOURCE\"`"              # relative
+TOP="`( cd \"$TOP\" && pwd )`"  # absolutized and normalized
+if [ -z "$TOP" ]
+then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+  
+logger "Found TOP at: $TOP"
+
+pushd "$TOP/src"
+BRANCH_NAME=`git status -bs --porcelain | head -n 1 | cut -d' ' -f2`
+find . -type f -print0 | xargs -0 perl -i.bak -pe 's/soapshop/$BRANCH_NAME/g'
+popd
