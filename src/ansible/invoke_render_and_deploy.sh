8013d45e1c995a82ea8a65ed9891a959db3d0879
Pieter Breed <github.com@pb.co.za>
2016-01-28 10:51:55 +0200

    added the invoke_render_and_deploy.sh command in the ansible src folder

---
+++ src/ansible/invoke_render_and_deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -x
+
+TOP="`dirname \"$BASH_SOURCE\"`"              # relative
+TOP="`( cd \"$TOP\" && pwd )`"  # absolutized and normalized
+if [ -z "$TOP" ]
+then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+  
+logger "Found TOP at: $TOP"
+
+export WEBSERVER_PUBLIC_DNSNAME=soapshop.pb.co.za
+export WEBSERVER_PUBLIC_PORT=443
+
+pushd "$TOP"
+
+pushd ../..
+render.sh
+popd
+
+ansible-playbook provision_web.yml \
+                 --tags=copy-html
+
+terminal-notifier -title "Done" -message "deploy finished" -appIcon ~"$TOP/../../output/hugo/apple-touch-icon.png -open "$WEBSERVER_PUBLIC_DNSNAME")
+
+popd
